f1744fdae6519b8866578bfeb54fe71c6a8d7897
ChenZheng <boch2436@gmail.com>
2016-12-07 11:36:20 +0800

    Initial Commit

---
+++ HelloXSC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloXSC
+//
+//  Created by ChenZheng on 2016/12/7.
+//  Copyright © 2016年 qsbk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloXSC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloXSC
+//
+//  Created by ChenZheng on 2016/12/7.
+//  Copyright © 2016年 qsbk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
