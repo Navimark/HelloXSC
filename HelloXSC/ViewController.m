5a2b9b60bc68af40d938f32f48966f67ee371a60
ChenZheng <boch2436@gmail.com>
2016-12-08 10:43:26 +0800

    test

--- HelloXSC/ViewController.m
+++ HelloXSC/ViewController.m
@@ -8,19 +8,29 @@
 
 #import "ViewController.h"
 #import "TestViewController.h"
+#import "YYLabel.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet YYLabel *mainLabel;
 
 @end
 
 @implementation ViewController
 - (IBAction)pushAction:(id)sender {
+    TestViewController *vc = [TestViewController new];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.title = @"(⊙o⊙)";
+    NSLog(@"label = %@",self.mainLabel);
+    self.mainLabel.text = @"∂ß≈ß¬…œ∂∑œø∑“œ®øπ˚ƒ…∂ß¬";
+    self.mainLabel.numberOfLines = 0;
+    self.mainLabel.textColor = [UIColor redColor];
+    [self.mainLabel.layer setBorderColor:[UIColor blueColor].CGColor];
+    [self.mainLabel.layer setBorderWidth:1];
 }
 
 
