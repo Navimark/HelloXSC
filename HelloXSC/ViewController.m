01d38d3140a0269450b24226c9ff4bc12cfa2632
ChenZheng <boch2436@gmail.com>
2016-12-12 11:40:47 +0800

    test

--- HelloXSC/ViewController.m
+++ HelloXSC/ViewController.m
@@ -13,16 +13,36 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet YYLabel *mainLabel;
 
+@property (nonatomic ) NSInteger index;
+
 @end
 
 @implementation ViewController
 - (IBAction)pushAction:(id)sender {
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"执行以下");
+    });
+    
+    self.index ++;
+    if (self.index % 3 == 1) {
+        NSLog(@"清楚，下次应该还会执行");
+        onceToken = 0;
+    } else {
+        NSLog(@"不清楚，下次xxx");
+    }
+    
+    
+    
+    
     TestViewController *vc = [TestViewController new];
     [self.navigationController pushViewController:vc animated:YES];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.index = 0;
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.title = @"(⊙o⊙)";
     NSLog(@"label = %@",self.mainLabel);
